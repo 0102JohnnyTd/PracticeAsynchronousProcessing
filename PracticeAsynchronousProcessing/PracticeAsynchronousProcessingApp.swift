19bac5f97a6ccd1f95a0ecfd953aedce86cd4ac3
0102johnny <rt2824.recruit.1022@gmail.com>
2023-04-07 22:51:13 +0900

    Initial commit

---
+++ PracticeAsynchronousProcessing/PracticeAsynchronousProcessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeAsynchronousProcessingApp.swift
+//  PracticeAsynchronousProcessing
+//
+//  Created by Johnny Toda on 2023/04/07.
+//
+
+import SwiftUI
+
+@main
+struct PracticeAsynchronousProcessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
