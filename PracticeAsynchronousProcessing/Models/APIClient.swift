fb586b460de0b42c79ca91d51832e6623bc031c0
0102johnny <rt2824.recruit.1022@gmail.com>
2023-04-09 09:47:17 +0900

    通信を実行して取得したデータをパースする処理を実装

--- PracticeAsynchronousProcessing/Models/APIClient.swift
+++ PracticeAsynchronousProcessing/Models/APIClient.swift
@@ -8,5 +8,29 @@
 import Foundation
 
 final class APIClient {
-   // 通信処理
+    // リクエストURLの文字列データ
+    private let requestURLString = "https://qiita.com/api/v2/items"
+    // 通信処理
+    func fetchArticleData(completion: @escaping (Result<[Article], Error>) -> Void) {
+        // リクエストURLを定義
+        guard let url = URL(string: requestURLString) else { fatalError("UnexpectedError") }
+        // DataTaskを定義
+        let dataTask = URLSession.shared.dataTask(with: url, completionHandler: { data, _, error in
+            if let error = error {
+                // 通信エラー時実行
+                completion(.failure(error))
+            }
+            guard let data = data else { fatalError("UnexpectedError") }
+            do {
+                let articlesList = try JSONDecoder().decode([Article].self, from: data)
+                // 通信、パース成功時に実行
+                completion(.success(articlesList))
+            } catch {
+                // パースエラー時に実行
+                completion(.failure(error))
+            }
+        })
+        // 通信を実行
+        dataTask.resume()
+    }
 }
